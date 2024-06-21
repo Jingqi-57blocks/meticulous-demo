10d48958f98caeb9871c9c4b95a3cdab518f4436
JingQi Zhang <jingqi@57blocks.com>
2024-06-21 13:48:45 +0800

    chore: add action

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,24 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 import App from './app/app';
+import { tryLoadAndStartRecorder } from '@alwaysmeticulous/recorder-loader';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+
+async function startApp() {
+
+  await tryLoadAndStartRecorder({
+    projectId: '8MOId59EctdjF8uekP4LZOTKo2Rm9CdB15gYJWQS',
+    isProduction: false,
+  });
+
+  const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+  );
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
+
+startApp();
